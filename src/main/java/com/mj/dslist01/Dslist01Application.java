310b1097a616e406ae086be9721fc75734862358
Marcio Gomes <mjgomes1978@gmail.com>
2025-08-22 22:04:20 -0300

    Project created.

---
+++ src/main/java/com/mj/dslist01/Dslist01Application.java
@@ -0,0 +1,13 @@
+package com.mj.dslist01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslist01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslist01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mj/dslist01/Dslist01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mj.dslist01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dslist01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
